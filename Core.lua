7dc96c065808c14464a4ca1a29034e2c5b25719a
-add <d.koukal@web.de>
2019-07-02 14:26:33 +0200

    Fix
    for whatever reason git has decided to delete some of the files...

---
+++ Core.lua
@@ -0,0 +1,170 @@
+AST                 = {}
+AST.name            = "AiMs-Synergy-Tracker"
+AST.author          = "AiMPlAyEr [EU]"
+AST.version         = "4.2"
+AST.website         = "http://www.esoui.com/downloads/info2084-AiMsSynergyTracker.html"
+AST.menuname        = "AiMs Synergy Tracker"
+AST.varVersion      = 7
+AST.default         = nil
+AST.varName         = "ASTSaved"
+
+local em            = EVENT_MANAGER
+local LibUnit       = LibStub:GetLibrary("LibUnits")
+
+local combat                = IsUnitInCombat("player")
+local wrapper, wrapper2     = nil, nil
+local fragment, fragment2   = nil, nil
+
+
+AST.default = {
+    ["orb"]         = true,
+    ["liq"]         = true,
+    ["pur"]         = true,
+    ["hea"]         = true,
+    ["bon"]         = false,
+    ["blo"]         = false,
+    ["tra"]         = false,
+    ["rad"]         = false,
+    ["cha"]         = false,
+    ["sha"]         = false,
+    ["imp"]         = false,
+    ["gra"]         = false,
+    ["hir"]         = false,
+    ["sol"]         = false,
+    ["rob"]         = false,
+    ["ago"]         = false,
+    ["windowstate"] = false,
+    ["windowscale"] = 1,
+    ["left"]        = 500,
+    ["top"]         = 500,
+    ["orientation"] = "horizontal",
+    ["alpha"]       = 0.8,
+    ["lockwindow"]  = false,
+    ["trackerui"]   = true,
+    ["healerui"]    = false,
+    ["interval"]    = 50,
+    ["textures"]    = false,
+    ["healer"]      = {
+        ["left"]            = 500,
+        ["top"]             = 500,
+        ["tanksonly"]       = false,
+        ["firstsynergy"]    = 1,
+        ["secondsynergy"]   = 2,
+        ["alpha"]           = 0.8,
+        ["windowscale"]     = 1,
+        ["ddsonly"]         = false,
+        ["ignoresynergy"]   = false,
+    }
+}
+
+function AST:Initialize()
+    em:RegisterForEvent(AST.name.."Combat", EVENT_PLAYER_COMBAT_STATE, AST.combatState)
+
+    for k, v in pairs(AST.Data.SynergyData) do
+        em:RegisterForEvent(AST.name.."Synergy"..k,EVENT_COMBAT_EVENT , AST.Tracking.synergyCheck)
+        em:AddFilterForEvent(AST.name.."Synergy"..k,EVENT_COMBAT_EVENT, REGISTER_FILTER_ABILITY_ID, k)
+    end
+
+    AST.SV = ZO_SavedVars:New(AST.varName, AST.varVersion, nil, AST.default)
+
+    if not AST.SV.healer.firstsynergy then AST.SV.healer.firstsynergy = 1 end
+    if not AST.SV.healer.secondsynergy then AST.SV.healer.secondsynergy = 2 end
+
+    AST.Tracker:Initialize(AST.SV.trackerui)
+    AST.Healer:Initialize(AST.SV.healerui)
+
+    if AST.SV.trackerui then wrapper, fragment = ASTGrid, ZO_SimpleSceneFragment:New(wrapper) end
+    if AST.SV.healerui then wrapper2, fragment2 = ASTHealerUI, ZO_SimpleSceneFragment:New(wrapper2) end
+
+    AST.LoadSettings()
+    AST.combatState()
+end
+
+function AST.windowState()
+    if AST.SV.windowstate then
+        if AST.SV.trackerui then
+            ASTGrid:SetHidden(true)
+            HUD_SCENE:RemoveFragment(fragment)
+            HUD_UI_SCENE:RemoveFragment(fragment)
+        end
+
+        if AST.SV.healerui then
+            ASTHealerUI:SetHidden(true)
+            HUD_SCENE:RemoveFragment(fragment2)
+            HUD_UI_SCENE:RemoveFragment(fragment2)
+        end
+
+        d(zo_strformat("|cfd6a02[AiMs Synergy Tracker]|r |cffffffTrackers are now <<1>> outside of combat", "|cdc143chidden|r"))
+    else
+        if AST.SV.trackerui then
+            ASTGrid:SetHidden(false)
+            HUD_SCENE:AddFragment(fragment)
+            HUD_UI_SCENE:AddFragment(fragment)
+        end
+
+        if AST.SV.healerui then
+            ASTHealerUI:SetHidden(false)
+            HUD_SCENE:AddFragment(fragment2)
+            HUD_UI_SCENE:AddFragment(fragment2)
+        end
+        d(zo_strformat("|cfd6a02[AiMs Synergy Tracker]|r |cffffffTrackers are now <<1>> outside of combat", "|c32cd32visible|r"))
+    end
+end
+
+function AST.combatState(event, inCombat)
+    if not AST.SV.windowstate then
+        if AST.SV.trackerui then
+            HUD_SCENE:AddFragment(fragment)
+            HUD_UI_SCENE:AddFragment(fragment)
+        end
+
+        if AST.SV.healerui then
+            HUD_SCENE:AddFragment(fragment2)
+            HUD_UI_SCENE:AddFragment(fragment2)
+        end
+    else
+        if inCombat ~= combat then
+            combat = inCombat
+            if inCombat then
+                if AST.SV.trackerui then ASTGrid:SetHidden(false) end
+                if AST.SV.healerui then ASTHealerUI:SetHidden(false) end
+            else
+                if AST.SV.trackerui then ASTGrid:SetHidden(true) end
+                if AST.SV.healerui then ASTHealerUI:SetHidden(true) end
+            end
+        end
+        if AST.SV.trackerui then
+            HUD_SCENE:RemoveFragment(fragment)
+            HUD_UI_SCENE:RemoveFragment(fragment)
+        end
+        if AST.SV.healerui then
+            HUD_SCENE:RemoveFragment(fragment2)
+            HUD_UI_SCENE:RemoveFragment(fragment2)
+        end
+    end
+end
+
+function AST.time(nd, numDecimalPlaces)
+    local mult = 10^(numDecimalPlaces or 0)
+	return math.floor((nd - GetGameTimeMilliseconds()/1000) * mult + 0.5)/mult
+end
+
+function AST.GetUnitName(unitId)
+    local unit = LibUnit:GetDisplayNameForUnitId(unitId)
+
+    if unit ~= "" or unit ~= nil then
+        return zo_strformat("<<1>>", unit)
+    else
+        return ""
+    end
+end
+
+SLASH_COMMANDS["/asttoggle"]   = AST.windowState
+
+function AST.OnAddOnLoaded(event, addonName)
+    if addonName ~= AST.name then return end
+    em:UnregisterForEvent(AST.name, EVENT_ADD_ON_LOADED)
+    AST:Initialize()
+end
+
+em:RegisterForEvent(AST.name, EVENT_ADD_ON_LOADED, AST.OnAddOnLoaded)
---
+++ Menu.lua
@@ -0,0 +1,446 @@
+function AST.LoadSettings()
+    local LAM = LibStub("LibAddonMenu-2.0")
+
+    local panelData = {
+        type = "panel",
+        name = AST.name,
+        displayName = AST.name,
+        author = "|cfd6a02AiMPlAyEr[EU]|r",
+        version = AST.version,
+        slashCommand = "/astmenu",
+        website = AST.website,
+        registerForRefresh = true,
+        registerForDefaults = true,
+    }
+    LAM:RegisterAddonPanel(AST.menuname, panelData)
+
+    local optionsTable = {}
+
+    --elements
+    table.insert(optionsTable, {
+        type = "header",
+        name = "Tracker Settings",
+    })
+
+    table.insert(optionsTable, {
+        type = "slider",
+        name = "Update Interval",
+        tooltip = "Defines the refresh rate in ms",
+        min = 50,
+        max = 1000,
+        step = 50,
+        getFunc = function() 
+            return AST.SV.interval
+        end,
+        setFunc = function(var) 
+            AST.SV.interval = var
+        end,
+    })
+
+    table.insert(optionsTable, {
+        type = "checkbox",
+        name = "Only shows in Combat",
+        tooltip = "Toggles the tracker outside of fights",
+        getFunc = function() return AST.SV.windowstate end,
+        setFunc = function(value) 
+            AST.SV.windowstate = value
+            windowstate = value
+            AST.windowState()
+        end,
+    })
+
+    table.insert(optionsTable, {
+        type = "checkbox",
+        name = "Lock UI Elements",
+        getFunc = function() return AST.SV.lockwindow end,
+        setFunc = function(value) 
+            AST.Tracker.SetWindowLock(value)
+            AST.SV.lockwindow = value 
+        end,
+    })
+
+    table.insert(optionsTable, {
+        type = "submenu",
+        name = "Synergy Settings",
+        controls = {
+            [1] = {
+                type = "description",
+                text = [[The |cff0000Tracker UI|r shows you your synergies and their cooldown.]],               
+            },
+            [2] = {
+                type = "checkbox",
+                name = "Tracker Frame",
+                tooltip = "Enables/Disables Tracker Frame",
+                getFunc = function() return AST.SV.trackerui end,
+                setFunc = function(value)
+                    AST.SV.trackerui = value
+                end,
+                requiresReload = true,
+            },
+            [3] = {
+                type = "dropdown",
+                name = "Alignment of Synergies",
+                tooltip = "Allows the alignment of synergies to be adjusted",
+                choices = {"horizontal", "vertical", "compact"},
+                getFunc = function() return AST.SV.orientation end,
+                setFunc = function(var) 
+                    AST.SV.orientation = var 
+                    AST.Tracker.UpdateElements()
+                end,
+                disabled = function() return not AST.SV.trackerui end,
+            },
+            [4] = {
+                type = "slider",
+                name = "Tracker Transparency",
+                tooltip = "Defines the transparency of the tracker's background.",
+                min = 0,
+                max = 100,
+                step = 1,
+                getFunc = function() 
+                    if AST.SV.alpha ~= nil then
+                        return math.floor(AST.SV.alpha*100) 
+                    end
+                end,
+                setFunc = function(var) 
+                    local newAlpha = var / 100
+                    AST.SV.alpha =newAlpha
+                    AST.LoadAlpha(newAlpha);
+                end,
+            },
+            [5] = {
+                type = "checkbox",
+                name = "Texture Transparency",
+                tooltip = "Makes the textures transparent as well",
+                getFunc = function() return AST.SV.textures end,
+                setFunc = function(value) 
+                    AST.SV.textures = value
+                    AST.Tracker.SetTrackerAlpha(AST.SV.alpha)
+                end,
+            },
+            [6] = {
+                type = "slider",
+                name = "Tracker Scale",
+                tooltip = "Allows to adjust the size of the Frame",
+                min = 50,
+                max = 200,
+                step = 1,
+                getFunc = function() 
+                    if AST.SV.windowscale ~= nil then
+                        return math.floor(AST.SV.windowscale*100) 
+                    end
+                end,
+                setFunc = function(var) 
+                    local newWindowScale = var / 100
+                    AST.SV.windowscale = newWindowScale
+                    AST.Tracker.UpdateElements()
+                end,
+            },
+            [7] = {
+                type = "divider",
+                height = 15,
+                alpha = 1,
+            },
+            [8] = {
+                type = "checkbox",
+                name = "Conduit",
+                tooltip = "Synergy of Liquid Lightning",
+                getFunc = function() return AST.SV.liq end,
+                setFunc = function(value) 
+                    AST.SV.liq = value 
+                    AST.Tracker.UpdateElements()
+                end,
+                disabled = function() return not AST.SV.trackerui end,
+            },
+            [9] = {
+                type = "checkbox",
+                name = "Purify",
+                tooltip = "Synergy of Cleansing Ritual",
+                getFunc = function() return AST.SV.pur end,
+                setFunc = function(value) 
+                    AST.SV.pur = value 
+                    AST.Tracker.UpdateElements()
+                end,
+                disabled = function() return not AST.SV.trackerui end,
+            },
+            [10] = {
+                type = "checkbox",
+                name = "Combustion/Blessed Shard",
+                tooltip = "Synergy of Energy Orb/Spear Shard",
+                getFunc = function() return AST.SV.orb end,
+                setFunc = function(value) 
+                    AST.SV.orb = value
+                    AST.Tracker.UpdateElements()
+                end,
+                disabled = function() return not AST.SV.trackerui end,
+            },
+            [11] = {
+                type = "checkbox",
+                name = "Bone Wall",
+                tooltip = "Synergy of Bone Shield",
+                getFunc = function() return AST.SV.bon end,
+                setFunc = function(value) 
+                    AST.SV.bon = value
+                    AST.Tracker.UpdateElements()
+                end,
+                disabled = function() return not AST.SV.trackerui end,
+            },
+            [12] = {
+                type = "checkbox",
+                name = "Harvest",
+                tooltip = "Synergy of Healing Seed",
+                getFunc = function() return AST.SV.hea end,
+                setFunc = function(value) 
+                    AST.SV.hea = value 
+                    AST.Tracker.UpdateElements()
+                end,
+                disabled = function() return not AST.SV.trackerui end,
+            },
+            [13] = {
+                type = "checkbox",
+                name = "Blood Funnel",
+                tooltip = "Synergy of Blood Altar",
+                getFunc = function() return AST.SV.blo end,
+                setFunc = function(value) 
+                    AST.SV.blo = value 
+                    AST.Tracker.UpdateElements()
+                end,
+                disabled = function() return not AST.SV.trackerui end,
+            },
+            [14] = {
+                type = "checkbox",
+                name = "Spawn Broodlings",
+                tooltip = "Synergy of Trapping Webs",
+                getFunc = function() return AST.SV.tra end,
+                setFunc = function(value) 
+                    AST.SV.tra = value 
+                    AST.Tracker.UpdateElements()
+                end,
+                disabled = function() return not AST.SV.trackerui end,
+            },
+            [15] = {
+                type = "checkbox",
+                name = "Charged Lightning",
+                tooltip = "Synergy of Summon Storm Atronach",
+                getFunc = function() return AST.SV.cha end,
+                setFunc = function(value) 
+                    AST.SV.cha = value 
+                    AST.Tracker.UpdateElements()
+                end,
+                disabled = function() return not AST.SV.trackerui end,
+            },
+            [16] = {
+                type = "checkbox",
+                name = "Radiate",
+                tooltip = "Synergy of Inner Fire",
+                getFunc = function() return AST.SV.rad end,
+                setFunc = function(value) 
+                    AST.SV.rad = value 
+                    AST.Tracker.UpdateElements()
+                end,
+                disabled = function() return not AST.SV.trackerui end,
+            },
+            [17] = {
+                type = "checkbox",
+                name = "Shackle",
+                tooltip = "Synergy of Dragonknight Standard",
+                getFunc = function() return AST.SV.sha end,
+                setFunc = function(value) 
+                    AST.SV.sha = value 
+                    AST.Tracker.UpdateElements()
+                end,
+                disabled = function() return not AST.SV.trackerui end,
+            },
+            [18] = {
+                type = "checkbox",
+                name = "Impale",
+                tooltip = "Synergy of Dark Talons",
+                getFunc = function() return AST.SV.imp end,
+                setFunc = function(value) 
+                    AST.SV.imp = value 
+                    AST.Tracker.UpdateElements()
+                end,
+                disabled = function() return not AST.SV.trackerui end,
+            },
+            [19] = {
+                type = "checkbox",
+                name = "Supernova",
+                tooltip = "Synergy of Nova",
+                getFunc = function() return AST.SV.gra end,
+                setFunc = function(value) 
+                    AST.SV.gra = value 
+                    AST.Tracker.UpdateElements()
+                end,
+                disabled = function() return not AST.SV.trackerui end,
+            },
+            [20] = {
+                type = "checkbox",
+                name = "Hidden Refresh",
+                tooltip = "Synergy of Consuming Darkness",
+                getFunc = function() return AST.SV.hir end,
+                setFunc = function(value) 
+                    AST.SV.hir = value
+                    AST.Tracker.UpdateElements()
+                end,
+                disabled = function() return not AST.SV.trackerui end,
+            },
+            [21] = {
+                type = "checkbox",
+                name = "Soul Leech",
+                tooltip = "Synergy of Soul Shred",
+                getFunc = function() return AST.SV.sol end,
+                setFunc = function(value) 
+                    AST.SV.sol = value
+                    AST.Tracker.UpdateElements() 
+                end,
+                disabled = function() return not AST.SV.trackerui end,
+            },
+            [22] = {
+                type = "checkbox",
+                name = "Grave Robber",
+                tooltip = "Synergy of Boneyard",
+                getFunc = function() return AST.SV.rob end,
+                setFunc = function(value) 
+                    AST.SV.rob = value
+                    AST.Tracker.UpdateElements() 
+                end,
+                disabled = function() return not AST.SV.trackerui end,
+            },
+            [23] = {
+                type = "checkbox",
+                name = "Pure Agony",
+                tooltip = "Synergy of Agony Totem",
+                getFunc = function() return AST.SV.ago end,
+                setFunc = function(value) 
+                    AST.SV.ago = value
+                    AST.Tracker.UpdateElements() 
+                end,
+                disabled = function() return not AST.SV.trackerui end,
+            },
+        },
+    })
+
+    table.insert(optionsTable, {
+        type = "submenu",
+        name = "Healer Settings",
+        controls = {
+            [1] = {
+                type = "description",
+                text = [[The |c32cd32Healer UI|r tracks Synergies being used by your tanks and dds.
+These setting to not affect the ones above.]],               
+            },
+            [2] = {
+                type = "checkbox",
+                name = "Healer Frame",
+                tooltip = "Enables/Disables Healer Frame",
+                getFunc = function() return AST.SV.healerui end,
+                setFunc = function(value)
+                    AST.SV.healerui = value
+                end,
+                requiresReload = true,
+            },
+            [3] = {
+                type = "slider",
+                name = "Healer UI Transparency",
+                tooltip = "Defines the transparency of the tracker's background.",
+                min = 0,
+                max = 100,
+                step = 1,
+                getFunc = function() 
+                    if AST.SV.healer.alpha ~= nil then
+                        return math.floor(AST.SV.healer.alpha*100) 
+                    end
+                end,
+                setFunc = function(var) 
+                    local newAlpha = var / 100
+                    AST.SV.healer.alpha =newAlpha
+                    AST.Healer.LoadAlpha(newAlpha);
+                end,
+            },
+            [4] = {
+                type = "slider",
+                name = "Tracker Scale",
+                tooltip = "Allows to adjust the size of the Frame",
+                min = 50,
+                max = 200,
+                step = 1,
+                getFunc = function() 
+                    if AST.SV.healer.windowscale ~= nil then
+                        return math.floor(AST.SV.healer.windowscale*100) 
+                    end
+                end,
+                setFunc = function(var) 
+                    local newWindowScale = var / 100
+                    AST.SV.healer.windowscale = newWindowScale
+                    AST.Healer.HealerUIUpdate()
+                end,
+                disabled = function() return not AST.SV.healerui end,
+            },
+            [5] = {
+                type = "checkbox",
+                name = "Only Track tanks",
+                tooltip = "DDs won't appear in the healer frame.",
+                getFunc = function() return AST.SV.healer.tanksonly end,
+                setFunc = function(value)
+                    AST.SV.healer.tanksonly = value
+                    AST.Healer.HealerUIUpdate()
+                end,
+                disabled = function() return not AST.SV.healerui end,
+            },
+            [6] = {
+                type = "checkbox",
+                name = "Only Track DDs",
+                tooltip = "Tanks won't appear in the healer frame.",
+                getFunc = function() return AST.SV.healer.ddsonly end,
+                setFunc = function(value)
+                    AST.SV.healer.ddsonly = value
+                    AST.Healer.HealerUIUpdate()
+                end,
+                disabled = function() return not AST.SV.healerui end,
+            },
+            [7] = {
+                type = "checkbox",
+                name = "Disable Second Synergy",
+                tooltip = "If this option is active, the second Synergy is hidden.",
+                getFunc = function() return AST.SV.healer.ignoresynergy end,
+                setFunc = function(value)
+                    AST.SV.healer.ignoresynergy = value
+                    AST.Healer.HealerUIUpdate()
+                end,
+                disabled = function() return not AST.SV.healerui end,
+            },
+            [8] = {
+                type = "divider",
+                height = 15,
+                alpha = 1,
+            },
+            [9] = {
+                type = "dropdown",
+                name = "First Synergy",
+                tooltip = "First trackable Synergy",
+                choices = D.SynergyList,
+                choicesValues = D.SynergyListValues,
+                getFunc = function() return AST.SV.healer.firstsynergy end,
+                setFunc = function(var) 
+                    AST.SV.healer.firstsynergy = var 
+                    AST.Healer.HealerUIUpdate()
+                end,
+                disabled = function() return not AST.SV.healerui end,
+            },
+            [10] = {
+                type = "dropdown",
+                name = "Second Synergy",
+                tooltip = "Second trackable Synergy",
+                choices = D.SynergyList,
+                choicesValues = D.SynergyListValues,
+                getFunc = function() return AST.SV.healer.secondsynergy end,
+                setFunc = function(var) 
+                    AST.SV.healer.secondsynergy = var
+                    AST.Healer.HealerUIUpdate()
+                end,
+                disabled = function() return not AST.SV.healerui end,
+            },
+        },
+    })
+
+    LAM:RegisterOptionControls(AST.menuname, optionsTable)
+end
